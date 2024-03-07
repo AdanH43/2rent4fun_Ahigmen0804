182e1b770af04678833bb9cab3f89eef6ae2113d
adan <adanhiguerasmendoza@gmail.com>
2024-03-07 17:49:50 +0100

    primera subida

--- models/renting_vehiculos.py
+++ models/renting_vehiculos.py
@@ -22,10 +22,6 @@ class RentingVehiculos(models.Model):
     #PARA CUANDO NO HAY UN ATRIBUTO LLAMADO NAME PARA MOSTRAR NOMBRE DE UN REGISTRO
     # https://www.odoo.com/es_ES/forum/ayuda-1/how-defined-display-name-in-custom-many2one-91657
     
-    #Indicamos que atributo sera el que se usara para mostrar nombre.
-    #Por defecto es "name", pero si no hay un atributo que se llama name, aqui lo indicamos
-    #Aqui indicamos que se use el atributo "nombre"
-    _rec_name = 'nombre'
 
     #Dato binario, para guardar un binario (en la vista indicaremos que es una imagen) con la portada del comic
     foto = fields.Image('Foto del vehiculo',max_width=50,max_height=50)
