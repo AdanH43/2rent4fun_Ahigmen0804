6d64b262d364be8db43daa4f2920bd1ab0c6e808
adan <adanhiguerasmendoza@gmail.com>
2024-03-07 17:54:33 +0100

    primera subida

--- models/renting_clientes.py
+++ models/renting_clientes.py
@@ -5,14 +5,14 @@ from odoo.exceptions import ValidationError
 
 class LigaPartido(models.Model):
     #Nombre y descripcion del modelo
-    _name = 'renting.cliente'
+    _name = 'renting.clientes'
     _description = 'Lista de Clientes'
 
 
     #Atributos del modelo
     nombre = fields.Char('Nombre', required=True, index=True)
     apellidos = fields.Char('Apellidos', required=True, index=True)
-    
+
     vehiculos_alquilados_ids = fields.Many2many('renting.vehiculos', string='Vehículos Alquilados')
 
     def name_get(self):
